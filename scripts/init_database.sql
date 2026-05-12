f61c006818097557a00ba99ddd517178311a0535
Ravina Shekhawat <183328992+ravina-shekhawat@users.noreply.github.com>
2026-05-12 20:24:23 +0530

    Initialize 'home' database and create schemas
    This script initializes the database 'home' and creates three schemas: bronze, silver, and gold.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,19 @@
-
+-- Create the database and the three schemas: bronze, silver, and gold.
+USE master;
+GO
+IF DB_ID('home') IS NOT NULL
+ALTER DATABASE home SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE IF EXISTS home;
+GO
+-- Create Database 'home'
+CREATE DATABASE home;
+GO 
+USE home;
+GO 
+-- Create Schema
+CREATE SCHEMA bronze ;
+GO
+CREATE SCHEMA silver ;
+GO
+CREATE SCHEMA gold ;
+GO
