882503d96a131bfc49a95d09d201a08599b74291
Ravina Shekhawat <183328992+ravina-shekhawat@users.noreply.github.com>
2026-05-16 17:30:22 +0530

    Create stored procedure for loading CSV data
    This script creates a stored procedure to automate the loading of CSV data into the bronze.orders table, truncating old data and displaying the loaded records.

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -1,1 +1,27 @@
+/*
+This script creates Stored Procedure to automate CSV data 
+loading  into bronze.orders table.
 
+It truncates old data, imports fresh data using BULK INSERT 
+and displays the loaded records.
+*/
+-- Creating Stored Procedure
+CREATE OR ALTER PROCEDURE bronze.bronze_load AS 
+BEGIN
+TRUNCATE TABLE bronze.orders; -- Truncates Old Data
+BULK INSERT bronze.orders   -- BULK INSERT
+FROM 'c:\tmp\train.csv'
+WITH (
+FORMAT = 'CSV',
+FIRSTROW = 2,
+ROWTERMINATOR = '0x0A',
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+SELECT * FROM bronze.orders -- Displays the data
+  Order by Row_ID ; -- Shorts Data in Ascending By Row_ID
+END;
+
+-- Exicute/Call the Stored Procedure
+use home;
+EXEC bronze.bronze_load;
