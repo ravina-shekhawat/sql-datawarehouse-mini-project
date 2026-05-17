198c9f20f276f0fb29ee6bf6d95eb5a055aa4291
Ravina Shekhawat <183328992+ravina-shekhawat@users.noreply.github.com>
2026-05-17 21:06:52 +0530

    Create stored procedure for loading silver orders

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -1,42 +1,66 @@
+/* 
+This script creates or alters a stored procedure to load data into the silver
+orders table from bronze.orders,
+including truncating the table before the insert.
+*/
 
-INSERT INTO silver.orders
-(
-Row_ID,
-Order_ID ,
-Order_Date ,
-Ship_Date ,
-Ship_Mode ,
-Customer_ID ,
-Customer_Name ,
-Segment ,
-Country ,
-City ,
-State ,
-Postal_Code ,
-Region ,
-Product_ID ,
-Category ,
-Sub_Category ,
-Product_Name ,
-Sales
-)
-SELECT 
-Row_ID,
-Order_ID ,
-TRY_CONVERT(DATE,Order_Date ,105),
-TRY_CONVERT(DATE, Ship_Date ,105),
-Ship_Mode ,
-Customer_ID ,
-Customer_Name ,
-Segment ,
-Country ,
-City ,
-State ,
-coalesce(Postal_Code,0) ,
-Region ,
-Product_ID ,
-Category ,
-Sub_Category ,
-Product_Name ,
-Sales
-from bronze.orders;
+CREATE OR ALTER PROCEDURE silver_load AS 
+    BEGIN
+      TRUNCATE TABLE silver.orders ;
+      INSERT INTO silver.orders
+       (
+          Row_ID,
+          Order_ID ,
+          Order_Date ,
+          Ship_Date ,
+          Ship_Mode ,
+          Customer_ID ,
+          Customer_Name ,
+          Segment ,
+          Country ,
+          City ,
+          State ,
+          Postal_Code ,
+          Region ,
+          Product_ID ,
+          Category ,
+          Sub_Category ,
+          Product_Name ,
+          Sales
+       )
+      SELECT 
+          Row_ID,
+          Order_ID ,
+          TRY_CONVERT(DATE,Order_Date ,105),
+          TRY_CONVERT(DATE, Ship_Date ,105),
+          Ship_Mode ,
+          Customer_ID ,
+          Customer_Name ,
+          Segment ,
+          Country ,
+          City ,
+          State ,
+          coalesce(Postal_Code,0) ,
+          Region ,
+          Product_ID ,
+          Category ,
+          Sub_Category ,
+          Product_Name ,
+          Sales
+          FROM bronze.orders;
+      SELECT * FROM silver.orders
+    END;
+    
+    -- Execute Stored Procedure
+    USE home; -- DB 
+    EXEC silver_load
+    
+
+
+
+
+
+
+
+
+
