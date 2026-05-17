774e4b3d0126c53619cb8d9f86019346dbe8dead
Ravina Shekhawat <183328992+ravina-shekhawat@users.noreply.github.com>
2026-05-17 17:56:39 +0530

    Insert data from bronze.orders into silver.orders

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -1,1 +1,42 @@
 
+INSERT INTO silver.orders
+(
+Row_ID,
+Order_ID ,
+Order_Date ,
+Ship_Date ,
+Ship_Mode ,
+Customer_ID ,
+Customer_Name ,
+Segment ,
+Country ,
+City ,
+State ,
+Postal_Code ,
+Region ,
+Product_ID ,
+Category ,
+Sub_Category ,
+Product_Name ,
+Sales
+)
+SELECT 
+Row_ID,
+Order_ID ,
+TRY_CONVERT(DATE,Order_Date ,105),
+TRY_CONVERT(DATE, Ship_Date ,105),
+Ship_Mode ,
+Customer_ID ,
+Customer_Name ,
+Segment ,
+Country ,
+City ,
+State ,
+coalesce(Postal_Code,0) ,
+Region ,
+Product_ID ,
+Category ,
+Sub_Category ,
+Product_Name ,
+Sales
+from bronze.orders;
