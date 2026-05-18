ecf168cb08e36e5b4ca2bc083ed68810499a7b37
Ravina Shekhawat <183328992+ravina-shekhawat@users.noreply.github.com>
2026-05-18 11:10:33 +0530

    Create view 'gold.orders' from 'silver.orders'
    This script creates a view in the Gold schema that selects various fields from the Silver orders table.

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,1 +1,25 @@
-
+/* 
+This script creates View in Gold Schema 
+*/
+IF OBJECT_ID('gold.orders','V') IS NOT NULL
+DROP VIEW gold.orders
+GO
+  CREATE VIEW gold.orders AS
+   SELECT 
+    Row_ID,
+    Order_ID,
+    Order_Date,
+    Ship_Date,
+    Ship_Mode,
+    Customer_ID,
+    Customer_Name,
+    Segment,
+    Country,
+    City,
+    State,
+    Region,
+    Product_ID,
+    Category,
+    Sub_Category,
+    Sales
+  FROM silver.orders	;
